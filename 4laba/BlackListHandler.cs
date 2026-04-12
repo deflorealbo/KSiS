0d38bdbb84ce891b59ddf06baf4bfbeae3fe8c27
deflorealbo <lena.2056@yandex.by>
2026-04-12 13:06:51 +0300

    4laba

---
+++ 4laba/BlackListHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleProxy
+{
+    public class BlackListHandler : IDisposable
+    {
+        private List<string> blacklist = new List<string>();
+
+        public BlackListHandler()
+        {
+            if (File.Exists("blacklist.txt"))
+            {
+                blacklist = new List<string>(File.ReadAllLines("blacklist.txt"));
+            }
+        }
+
+        public bool IsBlocked(string host)
+        {
+            foreach (var line in blacklist)
+            {
+                if (host.Contains(line.Trim()))
+                    return true;
+            }
+            return false;
+        }
+
+        public void WriteForbiddenMessage(NetworkStream stream)
+        {
+            string body = File.Exists("forbiddenPage.html")
+                ? File.ReadAllText("forbiddenPage.html")
+                : "<h1>403 Forbidden</h1>";
+
+            string response =
+                "HTTP/1.1 403 Forbidden\r\n" +
+                "Content-Type: text/html\r\n" +
+                $"Content-Length: {Encoding.UTF8.GetByteCount(body)}\r\n" +
+                "\r\n" +
+                body;
+
+            Console.WriteLine("Proxy: HTTP/1.1 403 Forbidden");
+            byte[] data = Encoding.UTF8.GetBytes(response);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ 4laba/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace simpleProxy
+{
+    class Program
+    {
+        private const int PORT = 5000;
+
+        static void Main()
+        {
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.2"), PORT);
+            listener.Start();
+
+            Console.WriteLine($"Proxy server started on port {PORT}");
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+
+                new Thread(() =>
+                {
+                    var handler = new ClientHandler(client);
+                    handler.Handle();
+                }).Start();
+            }
+        }
+    }
+}
